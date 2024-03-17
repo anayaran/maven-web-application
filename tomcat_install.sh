5e9a31cb2cb7bcf0088615d954920dedd6317ebe
anayaran <161417228+anayaran@users.noreply.github.com>
2024-03-17 16:47:51 +0530

    Update tomcat_install.sh

--- tomcat_install.sh
+++ tomcat_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum update
 sudo yum install java-11-amazon-corretto-devel -y
-wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.23/bin/apache-tomcat-10.0.23.tar.gz
-tar -xvf apache-tomcat-10*.tar.gz
-sudo mv apache-tomcat-10.0.23 /usr/local/tomcat
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.65/bin/apache-tomcat-9.0.65.tar.gz
+tar -xvf apache-tomcat-9*.tar.gz
+sudo mv apache-tomcat-9.0.65 /usr/local/tomcat
 sudo sh /usr/local/tomcat/bin/startup.sh
